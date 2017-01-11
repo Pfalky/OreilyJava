ca9ecea627197d4e3760581d801acdc937f0a8b1
pfalky <pfalky@yandex.ru>
2017-01-11 23:25:34 +0300

    FirstApp Class

---
+++ MyfirstApp/src/MyFirstApp.java
@@ -0,0 +1,9 @@
+/**
+ * Created by root on 11.01.2017.
+ */
+public class MyFirstApp {
+    public static void main(String[] args) {
+        System.out.println("Я управляю");
+        System.out.println("Миром");
+    }
+}
